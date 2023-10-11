e5d81c7588844069530b8221b98821d75139369e
gabecpf97 <gabrielcpf2997@gmail.com>
2023-10-10 21:48:20 -0700

    change status add to controller

--- controller/statusController.js
+++ controller/statusController.js
@@ -1,6 +1,27 @@
 import Status from "../models/status";
+import User from "../models/user";
 
-const changeStatus = (message) => {};
+const changeStatus = (user) => {
+  User.findById(user).exec((err, theUser) => {
+    if (err) {
+      return err;
+    } else if (!theUser) {
+      return new Error("User doesn't exist");
+    } else {
+      const theStatus = new Status({
+        user: theUser._id,
+        value: true,
+      });
+      theStatus.save((err) => {
+        if (err) {
+          return err;
+        } else {
+          return { theStatus };
+        }
+      });
+    }
+  });
+};
 
 const statusController = { changeStatus };
 
